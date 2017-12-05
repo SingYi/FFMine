f7929d853453a41808c5a8e74805051e2e29ed5e
Sing.Yi <23323231@qq.com>
2017-12-05 11:07:34 +0800

    Initial Commit

---
+++ FFLandmine/GameScene.h
@@ -0,0 +1,13 @@
+//
+//  GameScene.h
+//  FFLandmine
+//
+//  Created by 燚 on 2017/12/5.
+//  Copyright © 2017年 Yi Shi. All rights reserved.
+//
+
+#import <SpriteKit/SpriteKit.h>
+
+@interface GameScene : SKScene
+
+@end
---
+++ FFLandmine/GameViewController.h
@@ -0,0 +1,15 @@
+//
+//  GameViewController.h
+//  FFLandmine
+//
+//  Created by 燚 on 2017/12/5.
+//  Copyright © 2017年 Yi Shi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <SpriteKit/SpriteKit.h>
+#import <GameplayKit/GameplayKit.h>
+
+@interface GameViewController : UIViewController
+
+@end
